4455cbce19fc60437af95ba02ac6469d91214028
CardinalSilver <cardinalsilverpersonal@gmail.com>
2026-02-07 15:46:48 -0600

    Added numerical and categorical histograms. Chose not to include boxplots after identifying low variability in data

--- explorer.py
+++ explorer.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 file_path = "studentData.csv" # The path to the csv dataset
 df = pd.read_csv(file_path) # Load the dataset
@@ -8,23 +10,54 @@ df = pd.read_csv(file_path) # Load the dataset
 # FUNCTIONS #
 #############
 
-def featureDetails(col): 
+def featureDetails(col, graph=False): # Provides descriptive statistics for a given feature (col). Optional graph parameter defaulting to False
     print(f"\n{col}") # Print the feature name
     print(f"Type: {df[col].dtype}") # Print the type of the feature
     print(f"Missing: {df[col].isnull().sum()}") # Print the number of missing values
 
     if pd.api.types.is_numeric_dtype(df[col]): # Check for numeric features
         print(df[col].describe()) # Describe numeric features
+        if (graph): # If graph parameter is True, graph the feature
+            graphNumeric(col)
     else:
         print(df[col].value_counts(dropna=False).head(10)) # For categorical features, print the 10 most common values (including missing values)
+        if (graph): # If graph parameter is True, graph the feature
+            graphCategorical(col)
 
+def graphNumeric(col):
+    series = df[col].dropna() # Drop NA values
+
+    min_val = int(series.min()) # Find min
+    max_val = int(series.max()) # Find max
+    bins = np.arange(min_val - 0.5, max_val + 1.5, 1) # Produce edges such as 17.5-18.5 to capture values of 18; offers visual clarity
+
+    plt.figure() # Create figure
+    plt.hist(series, bins=bins, edgecolor="black", linewidth=2) # Plot a histogram using the created bins
+    plt.xticks(range(min_val, max_val + 1)) # Manually set the tickmarks to fit the integer range of the feature
+    plt.title(f"{col}: Histogram")
+    plt.xlabel(col)
+    plt.ylabel("Frequency")
+    plt.tight_layout() # Adjust spacing
+    plt.show()
+
+def graphCategorical(col):
+    series = df[col].value_counts(dropna=False).head(10) # Grab the 10 most frequent values
+
+    plt.figure() # Create figure
+    series.plot(kind="bar", edgecolor="black", linewidth=2) # Create bar graph
+    plt.title(f"{col}: Top 10 Categories")
+    plt.xlabel(col)
+    plt.ylabel("Count")
+    plt.xticks(rotation=45, ha="right") # Rotate labels and align to the right for visual clarity
+    plt.tight_layout() # Adjust spacing
+    plt.show()
 
 ##################
 # MAIN EXECUTION #
 ##################
 
 for col in df.columns:
-    featureDetails(col) # Get the feature details for each feature
+    featureDetails(col, True) # Get the feature details for each feature
 
 
 ###########
